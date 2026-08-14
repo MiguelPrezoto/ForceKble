137fcc6ca4cb78bdeea31c3b995a5816bd5fc925
keysteer <gustavomoreiracosta2008@gmail.com>
2026-08-14 10:44:42 -0300

    Update sobre.php

--- Arq/sobre.php
+++ Arq/sobre.php
@@ -1,1 +1,66 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Oficina Show Motors</title>
+    <link rel="stylesheet" href="style.css">
 
+    <link rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+</head>
+
+<style>
+
+.link{
+    text-decoration: none;
+    color: rgb(26, 26, 26);
+    background-color: aliceblue;
+    border-radius: 10px;
+    padding-inline: 20px;
+    padding-block: 3px;
+    font-weight: bold;
+    font-size: large;
+    
+}
+
+
+
+</style>
+<body>
+    <header class="topo">
+        <div class="lado-esquerdo">
+            <button class="botao-menu">
+                <i class="fa-solid fa-bars"></i>
+            </button>
+
+            <h1 class="titulo">Oficina Show Motors</h1>
+        </div>
+
+        <div class="usuario">
+            <div>
+
+                <a class="link" href="login.html">Login</a>
+
+            </div>
+
+            <div class="foto-usuario">
+                <i class="fa-solid fa-user"></i>
+            </div>
+        </div>
+    </header>
+    <Br></Br><center><h1>proga</h1></center>
+    <br><br>
+
+    <p class="fonte">Nos da Oficina SHOWMOTORS fundada em meados de 2006 como RK Dias, tínhamos na época um grande desafio, vender serviços de alta qualidade com peças originais de reposição em uma localidade já com dezenas de oficinas conceituadas na região. Foi então que começamos com serviços básicos de auto center e mecânica geral. Com o passar do tempo identificamos um mercado com grande carência de serviços altamente técnico e especializado para a demanda que surgira em um mercado aquecido no setor automotivo nacional.
+
+​<br><br><br><br><br>
+
+Desde então viemos investindo diretamente em conhecimento e especialização, a oficina SHOWMOTORS hoje possui grande acervo técnico e profissionais altamente qualificados prontos para atender os mais diversos tipos de tecnologia aplicada nos veículos nacionais e importados do mercados.
+
+​<br><br><br><br><br>
+
+Somos uma oficina referência na região, equipada com os mais diversos tipos de equipamentos que existentes no mercado, para atender os serviços de mecânica geral como embreagem, freios, suspensão, troca de óleo, limpeza de bicos entre outros, já dentro do contexto diagnostico e reparo eletrônico, prestamos serviços especializados em sistema de injeção eletrônica, reparos em módulos de injeção, programações de unidades eletrônicas, programação de chaves codificadas, sistemas ABS, EDB, EPS, Air Bag e todo o gerenciamento eletrônico de veículos automotores nacionais e importados.
+
+</p>
+</body>
